028303cf248ad7c483595fb7afa839bbd0671216
yangpu <yangpu@xiao100.com>
2020-12-04 16:41:18 +0800

    feat: 动态化vant

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <van-button type="primary" block>块级元素</van-button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -8,11 +9,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-
+import { Button } from 'vant'
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    [Button.name]: Button
   }
 })
 </script>
