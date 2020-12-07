9830ff90ebe2f572a3c8a328886dc54121e3e008
yangpu <yangpu@xiao100.com>
2020-12-07 15:57:20 +0800

    feat: 添加配置文件

--- packages/trial-student/vue.config.js
+++ packages/trial-student/vue.config.js
@@ -1,13 +1,50 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const { merge } = require('webpack-merge')
+const path = require('path')
 const tsImportPluginFactory = require('ts-import-plugin')
 
 module.exports = {
   parallel: false,
-  // outputDir: '../docs',
-  publicPath: process.env.NODE_ENV === 'production' ? '/trial-student/' : '/',
+
+  publicPath: process.env.VUE_APP_BASE_URL,
+  // outputDir: 'wechat',
+  productionSourceMap: false,
+
+  devServer: {
+    proxy: {
+      '/wechatV2': {
+        target: process.env.API,
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: { // 改变路径重定向
+          '^/wechatV2': '/wechat'
+        }
+      },
+      '/v2': {
+        target: process.env.API,
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: { // 改变路径重定向
+          '^/v2': '/'
+        }
+      }
+
+    }
+  },
+
+  css: {
+    loaderOptions: {
+      sass: {
+        // 会在所有的sass文件添加相同的代码，变量，mixins之类的，否则假设有20个文件被loader处理就有20个被导入，批量导入
+        // 但在被import的文件里面不会的
+        import: ['~@root/assets/scss/variable.scss']
+      }
+    }
+  },
+
   chainWebpack: config => {
+    config.resolve.alias.set('@root', path.resolve(__dirname, '../../'))
     config.module
       .rule('ts')
       .use('ts-loader')
@@ -30,4 +67,5 @@ module.exports = {
         return options
       })
   }
+
 }
