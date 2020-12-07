0927b0ae0747ad1e31d1f4b732d21770f93f0c28
yangpu <yangpu@xiao100.com>
2020-12-07 15:17:21 +0800

    feat: 添加类型别名

---
+++ common/const/type/course.ts
@@ -0,0 +1,17 @@
+import * as enums from '../enum';
+export interface ICourse {
+  readonly _id?: string;
+  name: string;
+  teacherId: string;
+  studentIds: string[];
+  status: enums.COURSE_STATUS;
+  desc: string;
+  day: enums.WEEK[]; // 一周
+  time: enums.DAY;
+  startDate: string;
+  endDate: string;
+  createDate?: string;
+  updateDate?: string;
+  endTime?: string;
+  startTime?: string;
+}
