c700d66577a1b947c1f2539506e2db6462fb96aa
yangpu <yangpu@xiao100.com>
2020-12-07 15:26:36 +0800

    feat: 添加api和类型定义

---
+++ common/api/hour.ts
@@ -0,0 +1,39 @@
+import {IHour} from '../const/type/hour';
+import { ApiListData, ApiResponse } from '../const/api';
+import http from '../utils/http';
+
+interface IResult {
+    templateMsg: {
+        errcode: number,
+    };
+    student_hour: {
+        n: number
+        nModified: number
+        ok: number,
+    };
+}
+
+
+/**
+ * 添加课程
+ * @param {*} params
+ */
+export function addHour(params: IHour): ApiResponse<IResult> {
+    return http.post('course-hour-flow', params);
+}
+
+/**
+ *
+ * @param params 查询参数
+ */
+export function getHourrList(params: QueryCondition<IHour>): ApiListData<IHour> {
+    return http.post('course-hour-flow/list', params);
+}
+
+/**
+ *
+ * @param id 课程的id
+ */
+export function getHour(id: string): ApiResponse<IHour> {
+    return http.get(`course-hour-flow/${id}`);
+}
---
+++ common/api/student-package.ts
@@ -0,0 +1,61 @@
+import {IStudentPackage, ICaculatePackage} from '../const/type/student-package';
+import { ApiListData, ApiResponse } from '../const/api';
+import http from '../utils/http';
+
+
+
+
+/**
+ * 添加学员课程包
+ * @param {*} params
+ */
+export function addStudentPackage(params: IStudentPackage) {
+    return http.post('student-package', params);
+}
+
+/**
+ *
+ * @param params 查询参数
+ */
+export function getStudentPackageList(params: any): ApiListData<IStudentPackage> {
+    return http.post('student-package/list-all', params);
+}
+
+/**
+ *
+ * @param params 查询参数
+ */
+export function getSimpleStudentPackageList(params: any): ApiListData<IStudentPackage> {
+    return http.post('student-package/simple-list', params);
+}
+
+/**
+ *
+ * @param id 学员课程包的id
+ */
+export function getPackage(id: string): ApiResponse<IStudentPackage> {
+    return http.get(`student-package/${id}`);
+}
+
+/**
+ *
+ * @param id 学员课程包id
+ * @param params
+ */
+export function updateStudentPackage(id: string, params: Partial<IStudentPackage>): ApiResponse<IStudentPackage> {
+    return http.put(`student-package/${id}`, params);
+}
+
+
+export function delStudentPackage(id: string): ApiResponse<IStudentPackage> {
+    return http.delete(`student-package/${id}`);
+}
+
+
+/**
+ * 学生毕业和在读的统计
+ * @param params
+ */
+export function caculatePackage(params?: any): ApiResponse<ICaculatePackage[]> {
+    return http.post(`statistics/caculatePackage`, params);
+  }
