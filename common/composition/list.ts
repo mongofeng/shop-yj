c33101681588593743c897d79c86a1b0125d11de
yangpu <yangpu@xiao100.com>
2020-12-09 11:43:58 +0800

    fix: 修复问题

--- common/composition/list.ts
+++ common/composition/list.ts
@@ -24,7 +24,7 @@ export function compositionList (func: any) {
   async function fetchData () {
     data.loading = true
     try {
-      const { data: { data: { list, count } } } = await func(condition)
+      const { data: { data: { list, count } } } = await func(condition.value)
       data.count = count
       data.page += 1
       data.list = data.list.concat(list)
@@ -33,9 +33,9 @@ export function compositionList (func: any) {
     }
   }
 
-  // onMounted(() => {
-  //   fetchData()
-  // })
+  onMounted(() => {
+    fetchData()
+  })
 
   function loadList () {
     if (data.list.length >= data.count) {
