cd3b5d51e1222ca03aebcc1dd73dabe57334e0a6
yangpu <yangpu@xiao100.com>
2020-12-16 16:25:38 +0800

    feat: 提取css

--- packages/bind-wechat-teacher/webpack.config.js
+++ packages/bind-wechat-teacher/webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 const path = require('path')
 
@@ -10,13 +11,14 @@ const config = {
   NODE_ENV: process.env.NODE_ENV,
   VUE_APP_API_SERVER: '/v2/'
 }
+
 // 使用node的模块
 module.exports = {
   // 这就是我们项目编译的入口文件
   entry: path.resolve(__dirname, './src/main.ts'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-[hash:8].js'
+    filename: '[name][hash].js'
   },
   resolve: {
     extensions: ['.ts', 'tsx', '.js'],
@@ -37,7 +39,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: [process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader']
       }
     ]
   },
@@ -75,6 +77,12 @@ module.exports = {
   // 这里就是一些插件
   plugins: [
     new webpack.DefinePlugin({ 'process.env': JSON.stringify(config) }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css'
+    }),
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [path.resolve(__dirname, './dist')]
     }),
